94cce14ba494ce3846170745bebdf0947ec5afeb
Tileryr <tileryrchicken@gmail.com>
2024-12-13 22:41:13 -0500

    randomly generated boids

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-let boidNumber = 1
+let boidNumber = 10
+let boidRadius = 10
+let boids = []
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 class Boid {
-    constructor({position, velocity, alignRadius, separateRadius}) {
+    constructor({position, velocity, alignRadius, separateRadius, angle}) {
         this.position = position
         this.velocity = velocity
         this.alignRadius = alignRadius
         this.separateRadius = separateRadius
+        this.angle = angle
     }
 
     draw() {
         ctx.strokeStyle = 'white'
         ctx.lineWidth = '3'
-        // ctx.strokeRect(this.position.x, this.position.y, 30, 30)
 
-        var triangle = makeTriangle(this.position, 10)
+        
+        this.angle = Math.atan2(this.velocity.y, this.velocity.x)
+        // console.log(this.velocity.y/this.velocity.x)
+        if (this.angle < 0) {this.angle+=Math.PI*2}
+        
+        var triangle = makeTriangle(this.position, boidRadius, this.angle)
 
         ctx.beginPath();
         ctx.moveTo(triangle.x1, triangle.y1);
@@ -32,29 +39,26 @@ class Boid {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
     }
 }
-
-let boid = new Boid({
-    position: {
-        x: 50,
-        y: 50
-    },
-    velocity: {
-        x: 10,
-        y: 10
-    }
-})
 
 function animate() {
     window.requestAnimationFrame(animate)
     ctx.fillStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
-    boid.update()
+
+    boids.forEach(boid => {
+        // console.log(boid)
+        boid.update()
+    })
 }
 
 function makeTriangle(position, radius, rotation = 0) {
+    // console.log(rotation)
     let number = (rotation/Math.PI)*3
+    console.log(number/3)
     let newTriangle = {
         //the first vertex is on the circumscribed circle at 0 radians where R is the radius of the circle ( R)
         //you may decide to change this.
@@ -68,7 +72,6 @@ function makeTriangle(position, radius, rotation = 0) {
         x3: position.x + radius * Math.cos((number+4)*Math.PI/3),
         y3: position.y + radius * Math.sin((number+4)*Math.PI/3)
     };
-    console.log(newTriangle)
     return newTriangle
 }
 
@@ -85,9 +88,22 @@ function debounce(func, timeout = 300) {
 function resizeWindow() {
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight  
-    playerWidth = widthPercent(playerPercent)
 }
 
 window.onresize = debounce(() => resizeWindow())
 
-window.onload = animate
+window.onload = () => {
+    for (let index = 0; index < boidNumber; index++) {
+        boids[index] = new Boid({
+            position: {
+                x: Math.random()*canvas.width,
+                y: Math.random()*canvas.height
+            },
+            velocity: {
+                x: Math.random()*10-5,
+                y: Math.random()*10-5
+            }
+        })
+    }
+    animate()
+}
